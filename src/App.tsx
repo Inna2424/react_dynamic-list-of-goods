cc42440b1e9d6527eb916517523d041e7591d305
Inna Haichenia <gajchenja2424@ukr.net>
2026-03-04 09:53:58 +0200

    add throw new Error in function getAdd, use React.memo in GoodList

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,53 @@ import { Good } from './types/Good';
 export const App: React.FC = () => {
   const [goodsFromServer, setGoodsFromServer] = React.useState<Good[]>();
 
-const loadAllGoods = () => {
-  getAll().then(setGoodsFromServer);
-}
+  const loadAllGoods = () => {
+    getAll().then(setGoodsFromServer);
+  };
 
-const loadFiveGoods = () => {
-  get5First().then(setGoodsFromServer);
-}
+  const loadFiveGoods = () => {
+    get5First().then(setGoodsFromServer);
+  };
 
-const handleLoadRed = () => {
+  const handleLoadRed = () => {
     getRedGoods().then(setGoodsFromServer);
   };
 
   return (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="all-button" onClick={() => {loadAllGoods()}}>
-      Load all goods
-    </button>
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => {
+          loadAllGoods();
+        }}
+      >
+        Load all goods
+      </button>
 
-    <button type="button" data-cy="first-five-button" onClick={() => {loadFiveGoods()}}>
-      Load 5 first goods
-    </button>
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => {
+          loadFiveGoods();
+        }}
+      >
+        Load 5 first goods
+      </button>
 
-    <button type="button" data-cy="red-button" onClick={() => {handleLoadRed()}}>
-      Load red goods
-    </button>
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => {
+          handleLoadRed();
+        }}
+      >
+        Load red goods
+      </button>
 
-    <GoodsList goods={goodsFromServer || []} />
-  </div>
-  )
-}
-
+      <GoodsList goods={goodsFromServer || []} />
+    </div>
+  );
+};
