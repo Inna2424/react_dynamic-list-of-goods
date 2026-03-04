2063766dde75fb41b013cec8c3e71632ae8adb82
Inna Haichenia <gajchenja2424@ukr.net>
2026-03-04 10:19:02 +0200

    add catch to App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,41 @@ import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [goodsFromServer, setGoodsFromServer] = React.useState<Good[]>();
+  const [errorMessage, setErrorMessage] = React.useState('');
 
   const loadAllGoods = () => {
-    getAll().then(setGoodsFromServer);
+    getAll()
+      .then(setGoodsFromServer)
+      .catch(() => {
+        setErrorMessage('Can not load goods');
+      });
   };
 
   const loadFiveGoods = () => {
-    get5First().then(setGoodsFromServer);
+    get5First()
+      .then(setGoodsFromServer)
+      .catch(() => {
+        setErrorMessage('Can not load goods');
+      });
   };
 
   const handleLoadRed = () => {
-    getRedGoods().then(setGoodsFromServer);
+    getRedGoods()
+      .then(setGoodsFromServer)
+      .catch(() => {
+        setErrorMessage('Can not load goods');
+      });
   };
 
   return (
     <div className="App">
       <h1>Dynamic list of Goods</h1>
+
+      {errorMessage && (
+        <p style={{ color: 'red' }} data-cy="error-message">
+          {errorMessage}
+        </p>
+      )}
 
       <button
         type="button"
